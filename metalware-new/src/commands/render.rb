7dd883a99697bbfde6de9a345979137e2f8df4c2
Bob Whitelock <bob.whitelock1@gmail.com>
2017-05-09 15:30:15 +0100

    Exclude unset parameters when templating
    Templater expects the hash passed to contain keys with set values, so
    filter out unset parameters otherwise leads to incorrect results. E.g.
    `Templater#get_yaml_file_list` will use a nil `nodename` when looking
    for config files, causing all config files to be included.

--- metalware-new/src/commands/render.rb
+++ metalware-new/src/commands/render.rb
@@ -6,9 +6,11 @@ module Metalware
     class Render
       def initialize(args, _options)
         template_path, maybe_node = args
-        templater = Templater::Combiner.new({
+
+        template_parameters = {
           nodename: maybe_node,
-        })
+        }.reject { |param, value| value.nil? }
+        templater = Templater::Combiner.new(template_parameters)
 
         rendered = templater.file(template_path)
         puts rendered
