2afbf00b154580f1b313e4c49806caf2c3ded3d0
Bob Whitelock <bob.whitelock1@gmail.com>
2017-05-09 16:08:26 +0100

    Implement `repo use` command
    This is a straight adaptation of some code from
    `Alces::Stack::Repo::Run#clone_repo` in
    `lib/ruby/lib/alces/stack/repo/run.rb`.

--- metalware-new/src/commands/repo/use.rb
+++ metalware-new/src/commands/repo/use.rb
@@ -1,10 +1,24 @@
 
+require 'rugged'
+require 'fileutils'
+
+require 'constants'
+
 module Metalware
   module Commands
     module Repo
       class Use
 	def initialize(args, options)
-	  puts "Running repo use with args #{args.inspect} and options #{options.inspect}"
+	  repo_url = args.first
+
+          if options.force
+            FileUtils::rm_rf Constants::REPO_PATH
+            # Alces::Stack::Log.info "Force deleted old repo"
+          end
+          Rugged::Repository.clone_at(repo_url, Constants::REPO_PATH)
+          # Alces::Stack::Log.info "Cloned '#{@opt.name_input}' from #{@opt.url}"
+        rescue Rugged::InvalidError
+	  raise $!, "Repository already exists. Use -f to force clone a new one"
 	end
       end
     end
