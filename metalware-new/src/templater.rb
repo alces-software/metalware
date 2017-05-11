a01949a103ab86da1b8f698d6dfbcd51e8269a49
Bob Whitelock <bob.whitelock1@gmail.com>
2017-05-11 14:51:22 +0100

    Refactor templater config parsing
    This is longer but hopefully clearer and will give better error
    messages, and in particular avoids the need to keep converting the
    config to a string and then `eval`ing this.

--- metalware-new/src/templater.rb
+++ metalware-new/src/templater.rb
@@ -154,17 +154,42 @@ module Metalware
 
       def parse_config
         current_parsed_config = base_config
-        current_str = current_parsed_config.to_s
-        old_str = ""
+        current_config_string = current_parsed_config.to_s
+        previous_config_string = nil
         count = 0
-        while old_str != current_str
+
+        # Loop through the config and recursively parse any config values which
+        # contain ERB, until the parsed config is not changing or we have
+        # exceeded the maximum number of passes to make.
+        while previous_config_string != current_config_string
           count += 1
           raise LoopErbError if count > Constants::MAXIMUM_RECURSIVE_CONFIG_DEPTH
-          old_str = current_str
-          current_str = replace_erb(current_str, current_parsed_config)
-          current_parsed_config = eval(current_str)
+
+          previous_config_string = current_config_string
+          current_parsed_config = perform_config_parsing_pass(current_parsed_config)
+          current_config_string = current_parsed_config.to_s
         end
+
         current_parsed_config
+      end
+
+      def perform_config_parsing_pass(current_parsed_config)
+        current_parsed_config.map do |k,v|
+          [k, parse_config_value(v, current_parsed_config)]
+        end.to_h
+      end
+
+      def parse_config_value(value, current_parsed_config)
+        case value
+        when String
+          replace_erb(value, current_parsed_config)
+        when Hash
+          value.map do |k,v|
+            [k, parse_config_value(v, current_parsed_config)]
+          end.to_h
+        else
+          value
+        end
       end
     end
 
