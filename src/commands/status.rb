2b2b4eb344e33090ae20e8c4c6c5468a54c411bb
william <william.mccumstie@outlook.com>
2017-06-05 18:27:00 +0100

    Update status to use the BaseCommand

--- src/commands/status.rb
+++ src/commands/status.rb
@@ -19,118 +19,91 @@
 # For more information on the Alces Metalware, please visit:
 # https://github.com/alces-software/metalware
 #==============================================================================
-require 'alces/tools/cli'
-require 'alces/stack/iterator'
-require 'alces/stack/log'
-require 'alces/stack/options'
-require 'alces/stack/status/monitor'
-require 'alces/stack/status/job'
+require 'base_command'
+require 'status/monitor'
+require 'status/job'
 require 'fileutils'
 
-module Alces
-  module Stack
-    class RunOptions < Alces::Stack::Options 
-      def cmds
-        [:power, :ping]
+module Metalware
+  module Commands
+    class Status < BaseCommand
+      def run
+        start_monitor
+        collect_data
+        @monitor.thread.join
+      end
+      
+      def start_monitor
+        @monitor = Alces::Stack::Status::Monitor.new({
+            nodes: @opt.nodes,
+            cmds: @opt.cmds,
+            thread_limit: @opt.thread_limit,
+            time_limit: @opt.time_limit
+          })
+        @monitor.start
       end
 
-      def nodes
-        @nodes ||= set_nodes
-      end
-
-      private
-      def set_nodes
-        lambda_proc = lambda { |lambda_hash| lambda_hash[:nodename] }
-        a = Alces::Stack::Iterator.run(group, lambda_proc, nodename: nodename)
-        a.is_a?(Array) ? a : [a]
-      end
-    end
-
-    module Status
-      class Run
-        def initialize(options={})
-          @opt = RunOptions.new(options)
-          raise "Requires either a nodename (-n) OR group (-g) input" if @opt.nodename? == @opt.group?
+      def collect_data
+        data = {}
+        empty_count = 0
+        while data["FINISHED"] != true
+          data = get_finished_data
+          if data.empty?
+            empty_count += 1
+            raise DataIncomplete if empty_count > 100 && @monitor.thread.stop?
+          elsif data["FINISHED"] != true
+            display_data data
+            empty_count = 0
+          end
+          sleep 1 # Only looks for updated data every second
         end
+      end
 
-        def run!
-          start_monitor
-          collect_data
-          @monitor.thread.join
+      class DataIncomplete < StandardError
+        def initialize(msg = "Failed to receive data for all nodes")
+          super
+        end
+      end
+
+      def display_data(data = {})
+        print_header unless @header_been_printed
+        
+        format_str = "%-10s"
+        printf format_str, data[:nodename]
+        @opt.cmds.each { |cmd| printf " | #{format_str}", data[cmd] }
+        puts
+      end
+
+      def print_header
+        @header_been_printed = true
+        header_data = {}
+        header_underline_hash = {}
+        header_underline_string = "----------"
+        header_data[:nodename] = "Node"
+        header_underline_hash[:nodename] = header_underline_string
+        @opt.cmds.each do |c|
+          header_data[c] = c.to_s.capitalize
+          header_underline_hash[c] = header_underline_string
+        end
+        display_data header_data
+        display_data header_underline_hash
+      end
+
+      def get_finished_data
+        @finished_node_index ||= 0
+        return {"FINISHED" => true} unless @finished_node_index < @opt.nodes.length
+        nodename = @opt.nodes[@finished_node_index]
+
+        current_results = Alces::Stack::Status::Job.results.tap do |r|
+          return {} if r.nil?
+          return {} unless r.key? nodename
+          r = r[nodename]
+          @opt.cmds.each { |cmd| return {} unless r.key? cmd }
+          r[:nodename] = nodename
         end
         
-        def start_monitor
-          @monitor = Alces::Stack::Status::Monitor.new({
-              nodes: @opt.nodes,
-              cmds: @opt.cmds,
-              thread_limit: @opt.thread_limit,
-              time_limit: @opt.time_limit
-            })
-          @monitor.start
-        end
-
-        def collect_data
-          data = {}
-          empty_count = 0
-          while data["FINISHED"] != true
-            data = get_finished_data
-            if data.empty?
-              empty_count += 1
-              raise DataIncomplete if empty_count > 100 && @monitor.thread.stop?
-            elsif data["FINISHED"] != true
-              display_data data
-              empty_count = 0
-            end
-            sleep 1 # Only looks for updated data every second
-          end
-        end
-
-        class DataIncomplete < StandardError
-          def initialize(msg = "Failed to receive data for all nodes")
-            super
-          end
-        end
-
-        def display_data(data = {})
-          print_header unless @header_been_printed
-          
-          format_str = "%-10s"
-          printf format_str, data[:nodename]
-          @opt.cmds.each { |cmd| printf " | #{format_str}", data[cmd] }
-          puts
-        end
-
-        def print_header
-          @header_been_printed = true
-          header_data = {}
-          header_underline_hash = {}
-          header_underline_string = "----------"
-          header_data[:nodename] = "Node"
-          header_underline_hash[:nodename] = header_underline_string
-          @opt.cmds.each do |c|
-            header_data[c] = c.to_s.capitalize
-            header_underline_hash[c] = header_underline_string
-          end
-          display_data header_data
-          display_data header_underline_hash
-        end
-
-        def get_finished_data
-          @finished_node_index ||= 0
-          return {"FINISHED" => true} unless @finished_node_index < @opt.nodes.length
-          nodename = @opt.nodes[@finished_node_index]
-
-          current_results = Alces::Stack::Status::Job.results.tap do |r|
-            return {} if r.nil?
-            return {} unless r.key? nodename
-            r = r[nodename]
-            @opt.cmds.each { |cmd| return {} unless r.key? cmd }
-            r[:nodename] = nodename
-          end
-          
-          @finished_node_index += 1
-          current_results[nodename]
-        end
+        @finished_node_index += 1
+        current_results[nodename]
       end
     end
   end
