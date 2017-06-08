38c37340559bc41682a3ebf6ddf82c12124b76bf
william <william.mccumstie@outlook.com>
2017-06-08 13:48:08 +0100

    Update Status::Job to use new location of power
    Now that the power command script has been moved to libexec, it makes
    more sense to run the script directly instead of reinvoking another
    ruby process and accessing it through the CLI.

--- src/status/job.rb
+++ src/status/job.rb
@@ -21,6 +21,7 @@
 #==============================================================================
 require 'metal_log'
 require 'timeout'
+require 'constants'
 
 module Metalware
   module Status
@@ -92,7 +93,8 @@ module Metalware
       end
 
       def job_power_status
-        cmd = "#{@metal} power #{@nodename} status 2>&1"
+        script = File.join(Constants::METALWARE_INSTALL_PATH, "libexec/power")
+        cmd = "#{script} #{@nodename} status 2>&1"
         result = run_bash(cmd)
                   .scan(/Chassis Power is .*\Z/)[0].to_s
                   .scan(Regexp.union(/on/, /off/))[0]
