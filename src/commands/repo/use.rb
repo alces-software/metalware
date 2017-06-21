660ba85ee45f4b369dd57a414d3448d5ce057e38
william <william.mccumstie@outlook.com>
2017-06-21 18:31:19 +0100

    Fix bug causing repo url not to be logged

--- src/commands/repo/use.rb
+++ src/commands/repo/use.rb
@@ -21,7 +21,7 @@ module Metalware
           end
 
           Rugged::Repository.clone_at(@repo_url, config.repo_path)
-          MetalLog.info "Cloned repo from #{@options.url}"
+          MetalLog.info "Cloned repo from #{@repo_url}"
         rescue Rugged::InvalidError
           raise $!, "Repository already exists. Use -f to force clone a new one"
         end
