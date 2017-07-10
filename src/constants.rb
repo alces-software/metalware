489caaa6a37ea03990664a9b79c24a902750fea7
Bob Whitelock <bob.whitelock1@gmail.com>
2017-07-10 13:07:39 +0100

    Extract groups cache path to constants

--- src/constants.rb
+++ src/constants.rb
@@ -30,6 +30,7 @@ module Metalware
     METALWARE_DATA_PATH = '/var/lib/metalware'
     CACHE_PATH = File.join(METALWARE_DATA_PATH, 'cache')
     HUNTER_PATH = File.join(CACHE_PATH, 'hunter.yaml')
+    GROUPS_CACHE_PATH = File.join(CACHE_PATH, 'groups.yaml')
 
     MAXIMUM_RECURSIVE_CONFIG_DEPTH = 10
 
