670a8d3611c7cafb96f0e47d510a91ac951072ea
Bob Whitelock <bob.whitelock1@gmail.com>
2017-07-17 10:34:57 +0100

    Ensure genders file is rendered before hosts
    Genders file needs to be rendered first, as how this is rendered will
    effect the groups and nodes used when rendering the hosts file.

--- spec/command_helpers/configure_command_spec.rb
+++ spec/command_helpers/configure_command_spec.rb
@@ -32,17 +32,19 @@ RSpec.describe Metalware::CommandHelpers::ConfigureCommand do
         {TEST_COMMAND_NAME => []}
       )
 
+      # Genders file needs to be rendered first, as how this is rendered will
+      # effect the groups and nodes used when rendering the hosts file.
       expect(Metalware::Templater).to receive(:render_to_file).with(
           instance_of(Metalware::Config),
           '/var/lib/metalware/repo/genders/default',
           Metalware::Constants::GENDERS_PATH
-      ).and_call_original
+      ).ordered.and_call_original
 
       expect(Metalware::Templater).to receive(:render_to_file).with(
           instance_of(Metalware::Config),
           '/var/lib/metalware/repo/hosts/default',
           Metalware::Constants::HOSTS_PATH
-      ).and_call_original
+      ).ordered.and_call_original
 
       SpecUtils.run_command(TestCommand)
     end
