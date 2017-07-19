89303bd096516b92cee49ae7c439798c183e5064
Bob Whitelock <bob.whitelock1@gmail.com>
2017-07-19 11:40:08 +0100

    Handle passing validation block to render_to_file
    If a block is given, pass it the rendered template and it should return
    whether this is valid and should be written to the file.
    
    This is intended for use when rendering the genders file, so we can
    validate it before installing it; it may also be useful for rendering
    the dhcp template or others.

--- spec/templater_spec.rb
+++ spec/templater_spec.rb
@@ -196,15 +196,36 @@ RSpec.describe Metalware::Templater do
     let :output_path { '/output' }
     let :output { File.read(output_path) }
 
-    it 'renders the template to the file' do
+    def render_to_file_with_block(&block)
+      Metalware::Templater.render_to_file(
+        config,
+        template_path,
+        output_path,
+        &block
+      )
+    end
+
+    it 'renders the template to the file by default' do
       filesystem.test do
-        Metalware::Templater.render_to_file(
-          config,
-          template_path,
-          output_path
-        )
+        render_to_file_with_block
 
         expect(output).to eq(template)
+      end
+    end
+
+    it 'renders template to the file if passed a block with truthy output' do
+      filesystem.test do
+        render_to_file_with_block(&:present?)
+
+        expect(output).to eq(template)
+      end
+    end
+
+    it 'does not render template to the file if passed a block with falsy output' do
+      filesystem.test do
+        render_to_file_with_block(&:empty?)
+
+        expect(File.exist?(output_path)).to be false
       end
     end
   end
