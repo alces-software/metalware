e7a30a0113d001f7485e6aaad6eb7a470b1ebfbc
Bob Whitelock <bob.whitelock1@gmail.com>
2017-07-19 14:18:58 +0100

    Group NodeattrInterface tests which mock genders
    Move all the current tests to a new `context` block where a fixtures
    genders file is used for running `nodeattr` commands; this is in
    preparation for testing a new function where we do not want to do this.

--- spec/nodeattr_interface_spec.rb
+++ spec/nodeattr_interface_spec.rb
@@ -27,45 +27,47 @@ require 'spec_utils'
 require 'exceptions'
 
 RSpec.describe Metalware::NodeattrInterface do
-  before do
-    SpecUtils.use_mock_genders(self)
-  end
-
-  describe '#nodes_in_group' do
-    it 'returns names of all nodes in the given gender group' do
-      expect(
-        Metalware::NodeattrInterface.nodes_in_group('masters')
-      ).to eq(['login1'])
-      expect(
-        Metalware::NodeattrInterface.nodes_in_group('nodes')
-      ).to eq(['testnode01', 'testnode02', 'testnode03'])
-      expect(
-        Metalware::NodeattrInterface.nodes_in_group('domain')
-      ).to eq(['login1', 'testnode01', 'testnode02', 'testnode03'])
+  context 'using mock genders' do
+    before do
+      SpecUtils.use_mock_genders(self)
     end
 
-    it 'raises if cannot find gender group' do
-      expect do
-        Metalware::NodeattrInterface.nodes_in_group('non_existent')
-      end.to raise_error Metalware::NoGenderGroupError
-    end
-  end
+    describe '#nodes_in_group' do
+      it 'returns names of all nodes in the given gender group' do
+        expect(
+          Metalware::NodeattrInterface.nodes_in_group('masters')
+        ).to eq(['login1'])
+        expect(
+          Metalware::NodeattrInterface.nodes_in_group('nodes')
+        ).to eq(['testnode01', 'testnode02', 'testnode03'])
+        expect(
+          Metalware::NodeattrInterface.nodes_in_group('domain')
+        ).to eq(['login1', 'testnode01', 'testnode02', 'testnode03'])
+      end
 
-  describe '#groups_for_node' do
-    it 'returns groups for given node, ordered as in genders' do
-      testnode_groups = ['testnodes', 'nodes', 'cluster', 'domain']
-      expect(
-        Metalware::NodeattrInterface.groups_for_node('testnode01')
-      ).to eq(testnode_groups)
-      expect(
-        Metalware::NodeattrInterface.groups_for_node('testnode02')
-      ).to eq(['pregroup'] + testnode_groups + ['postgroup'])
+      it 'raises if cannot find gender group' do
+        expect do
+          Metalware::NodeattrInterface.nodes_in_group('non_existent')
+        end.to raise_error Metalware::NoGenderGroupError
+      end
     end
 
-    it 'raises if cannot find node' do
-      expect do
-        Metalware::NodeattrInterface.groups_for_node('non_existent')
-      end.to raise_error Metalware::NodeNotInGendersError
+    describe '#groups_for_node' do
+      it 'returns groups for given node, ordered as in genders' do
+        testnode_groups = ['testnodes', 'nodes', 'cluster', 'domain']
+        expect(
+          Metalware::NodeattrInterface.groups_for_node('testnode01')
+        ).to eq(testnode_groups)
+        expect(
+          Metalware::NodeattrInterface.groups_for_node('testnode02')
+        ).to eq(['pregroup'] + testnode_groups + ['postgroup'])
+      end
+
+      it 'raises if cannot find node' do
+        expect do
+          Metalware::NodeattrInterface.groups_for_node('non_existent')
+        end.to raise_error Metalware::NodeNotInGendersError
+      end
     end
   end
 end
