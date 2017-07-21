83e1b3a0258f451a75226940dd1e6ddb64cbce8b
Bob Whitelock <bob.whitelock1@gmail.com>
2017-07-21 16:44:50 +0100

    Do not colourize when piping view-config output

--- src/commands/view_config.rb
+++ src/commands/view_config.rb
@@ -28,11 +28,17 @@ module Metalware
 
       def pretty_print_json(json)
         # Delegate pretty printing with colours to `jq`.
-        Open3.popen2('jq . --color-output') do |stdin, stdout|
+        Open3.popen2(jq_command) do |stdin, stdout|
           stdin.write(json)
           stdin.close
           puts stdout.read
         end
+      end
+
+      def jq_command
+        # Do not colourize the output if we're not outputting to a terminal, so
+        # don't get escape sequences piped to next command.
+        "jq . #{STDOUT.isatty ? '--color-output' : ''}"
       end
     end
   end
