afb7d21b7629cde49a7e612e3c686fc53ccf2654
william <william.mccumstie@outlook.com>
2017-07-31 12:24:47 +0100

    Create remove group command
    This commit contains the initial code required for removing a primary
    group. Due to the complexity in testing this operation a fixture
    set has been created, which contains all the files required to test
    how the group is removed.

---
+++ src/commands/remove/group.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+#==============================================================================
+# Copyright (C) 2017 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Metalware.
+#
+# Alces Metalware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Metalware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on the Alces Metalware, please visit:
+# https://github.com/alces-software/metalware
+#==============================================================================
+
+module Metalware
+  module Commands
+    module Remove
+      class Group < CommandHelpers::BaseCommand
+        def setup(args, _options)
+          @group = args[0]
+        end
+
+        def run
+          delete_answer_files
+        end
+
+        private
+
+        attr_reader :group
+
+        def delete_answer_files; end
+
+        def list_of_answer_files; end
+      end
+    end
+  end
+end
