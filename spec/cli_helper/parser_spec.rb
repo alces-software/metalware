c9857c076a7df181a1f83eadbf36a637edcc0ea1
Bob Whitelock <bob.whitelock1@gmail.com>
2017-08-01 18:57:58 +0100

    Add test for existing Parser option parsing
    I'm about to add support for defining command options with dynamic
    defaults, so makes sense to add a test for the existing behaviour.
    
    To do this needed to extract the hard-coded config path to a constant,
    so this can be stubbed to use our own config.

---
+++ spec/cli_helper/parser_spec.rb
@@ -0,0 +1,45 @@
+
+# frozen_string_literal: true
+
+RSpec.describe Metalware::CliHelper::Parser do
+  subject do
+    Metalware::CliHelper::Parser.new(
+      Metalware::Cli.new
+    )
+  end
+
+  before do
+    stub_const('Metalware::CliHelper::CONFIG_PATH', test_config_path)
+  end
+
+  let :test_config_path { '/tmp/config.yaml' }
+
+  describe 'default parsing' do
+    def define_config_with_default(default_value)
+      File.write(test_config_path, YAML.dump(
+                                     'commands' => {
+                                       'my_command' => {
+                                         'options' => [{
+                                           'tags' => ['-f', '--foo'],
+                                           'default' => default_value,
+                                         }],
+                                       },
+                                     },
+                                     'global_options' => {}
+      ))
+    end
+
+    it 'passes simple default values straight through as option default' do
+      define_config_with_default(5)
+
+      expect_any_instance_of(Commander::Command).to receive(:option).with(
+        '-f', '--foo', # tags
+        nil, # type
+        { default: 5 }, # default
+        '' # description
+      )
+
+      subject.parse_commands
+    end
+  end
+end
