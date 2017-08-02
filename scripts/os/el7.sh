7a8f7ca3401949762495302c69d7e2ecfdbb1f36
Bob Whitelock <bob.whitelock1@gmail.com>
2017-08-02 15:18:19 +0100

    Install syslinux (for gethostip) in installer

--- scripts/os/el7.sh
+++ scripts/os/el7.sh
@@ -29,6 +29,7 @@ install_runtime_prerequisites() {
         yum -e0 -y install gettext && \
         yum -e0 -y install epel-release && \
         yum -e0 -y install jq && \
+        yum -e0 -y install syslinux && \
         yum -e0 -y install git
 }
 
