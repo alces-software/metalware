f1612326719ba1b978a12fc435b507feb8141cf9
Bob Whitelock <bob.whitelock1@gmail.com>
2017-08-11 17:16:53 +0100

    Require CLI from Rails app
    So can access all CLI classes throughout the app.

--- src/gui/config/application.rb
+++ src/gui/config/application.rb
@@ -14,6 +14,8 @@ require 'action_cable/engine'
 require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
+require_relative '../../cli'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
