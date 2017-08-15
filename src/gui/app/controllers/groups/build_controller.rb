777c1621f9554f634efc0a4cc43c91a38cef0b61
Bob Whitelock <bob.whitelock1@gmail.com>
2017-08-15 13:56:09 +0100

    rubocop --auto-correct some things

--- src/gui/app/controllers/groups/build_controller.rb
+++ src/gui/app/controllers/groups/build_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Groups::BuildController < ApplicationController
   def show
     @name = params[:group_id]
--- src/gui/app/controllers/nodes/build_controller.rb
+++ src/gui/app/controllers/nodes/build_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Nodes::BuildController < ApplicationController
   def show
     @name = params[:node_id]
