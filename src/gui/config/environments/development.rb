a4f569e331aa6a9082fadd8fde7caed8ffe6ebb6
Bob Whitelock <bob.whitelock1@gmail.com>
2017-08-18 14:37:18 +0100

    Expand IP whitelist to include VPN network

--- src/gui/config/environments/development.rb
+++ src/gui/config/environments/development.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Allow access from within same network as Controller appliance.
-  config.web_console.whitelisted_ips = '10.100.0.0/16'
+  config.web_console.whitelisted_ips = '10.0.0.0/8'
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
