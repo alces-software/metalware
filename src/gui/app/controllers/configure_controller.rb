7c3fb7815f472d21bfc4498fa2000e0e7fde69c6
Bob Whitelock <bob.whitelock1@gmail.com>
2017-08-18 13:17:35 +0100

    Use inheritance for Domain Configure controller
    This is the first step of sharing behaviour between the configure
    process for the domain, groups, and nodes.

---
+++ src/gui/app/controllers/configure_controller.rb
@@ -0,0 +1,42 @@
+
+# frozen_string_literal: true
+
+class ConfigureController < ApplicationController
+  def show
+    assign_form_variables
+  end
+
+  def create
+    if configure_with_answers(entered_answers)
+      redirect_to '/'
+    else
+      assign_form_variables(entered_answers: entered_answers)
+      render 'show'
+    end
+  end
+
+  private
+
+  def assign_form_variables
+    @title = 'Configure Domain'
+    @questions = Configure::Questions.for_domain
+
+    # If we've been sent some `answers` in the request then these will be
+    # re-shown in the form, otherwise we will show the old saved answers.
+    entered_answers if params[:answers]
+  end
+
+  def entered_answers
+    @entered_answers ||=
+      params[:answers].permit!.to_h.select do |_identifier, answer|
+        answer.present?
+      end
+  end
+
+  def configure_with_answers(answers)
+    run_command(
+      Metalware::Commands::Configure::Domain,
+      answers: answers.to_json
+    )
+  end
+end
