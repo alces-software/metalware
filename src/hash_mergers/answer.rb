748b211d0361f3a8a66d8f34c3c1a9807ebe1e3a
william <william.mccumstie@outlook.com>
2017-09-27 12:19:33 +0100

    Make HashMerger methods private

--- src/hash_mergers/answer.rb
+++ src/hash_mergers/answer.rb
@@ -6,6 +6,7 @@ require 'hash_mergers/hash_merger'
 module Metalware
   module HashMergers
     class Answer < HashMerger
+      private
       def load_yaml(section, section_name = nil)
         input = (section_name ? [section_name] : [])
         loader.section_answers(section, *input)
--- src/hash_mergers/config.rb
+++ src/hash_mergers/config.rb
@@ -6,6 +6,7 @@ require 'hash_mergers/hash_merger'
 module Metalware
   module HashMergers
     class Config < HashMerger
+      private
       def load_yaml(section, section_name = nil)
         input = (section_name ? [section_name] : [])
         Data.load(file_path.send("#{section}_config", *input))
