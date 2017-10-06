84dcd084de9e25f23e068d09d3f97025770e427f
william <william.mccumstie@outlook.com>
2017-10-06 15:42:07 +0100

    Improve speed by file caching in HashMerger
    Once a file has been loaded, in HashMerger it is saved into Memory
    so it can be reused. The file cache is not global as it only applies
    to the HashMerge object.
    
    However this does mean the answer and config files do not need to be
    reloaded between nodes.

--- src/hash_mergers/hash_merger.rb
+++ src/hash_mergers/hash_merger.rb
@@ -13,6 +13,7 @@ module Metalware
         @metalware_config = metalware_config
         @file_path = FilePath.new(metalware_config)
         @loader = Validation::Loader.new(metalware_config)
+        @cache = {}
       end
 
       def merge(groups: [], node: nil, &templater_block)
@@ -23,20 +24,51 @@ module Metalware
 
       private
 
-      attr_reader :metalware_config, :file_path, :loader
+      attr_reader :metalware_config, :file_path, :loader, :cache
 
       ##
-      # hash_array enforces the order in which the hashes are loaded, it is not
-      # responsible for how the file is loaded as that is delegated to load_yaml
+      # hash_array enforces the order in which the hashes are loaded, it is
+      # not responsible for how the file is loaded as that is delegated to
+      # load_yaml
       #
       def hash_array(groups:, node:)
-        [load_yaml(:domain)].tap do |arr|
-          groups.each { |group| arr.push(load_yaml(:group, group)) }
-          arr.push(load_yaml(:node, node)) if node
+        [cached_yaml(:domain)].tap do |arr|
+          groups.each { |group| arr.push(cached_yaml(:group, group)) }
+          arr.push(cached_yaml(:node, node)) if node
         end
       end
 
-      def load_yaml(_section, _section_name = nil)
+      def cached_yaml(section, section_name = nil)
+        data = cached_data(section, section_name)
+        return data if data
+        data = load_yaml(section, section_name)
+        add_cached_data(section, section_name, data)
+        data
+      end
+
+      def cached_data(section, section_name)
+        if section_name
+          cache_section = cache[section]
+          return nil unless cache_section
+          cache_section[section_name]
+        else
+          no_section_name = cache[:no_section_name]
+          return nil unless no_section_name
+          no_section_name[section]
+        end
+      end
+
+      def add_cached_data(section, section_name, data)
+        if section_name
+          cache[section] = {} unless cache[section]
+          cache[section][section_name] = data
+        else
+          cache[:no_section_name] = {} unless cache[:no_section_name]
+          cache[:no_section_name][section] = data
+        end
+      end
+
+      def load_yaml(_section, _section_name)
         raise NotImplementedError
       end
 
