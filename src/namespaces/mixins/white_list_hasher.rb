980632f29d5794f410bdaa9f9b380155085858eb
william <william.mccumstie@outlook.com>
2017-10-23 15:03:20 +0100

    Clean up the WhiteListHasher code
    The memo does not need to be explicitly returned at the end of the
    each_with_object block. Also the two white list iterator methods
    should have the same input to improve consitancy.

--- src/namespaces/mixins/white_list_hasher.rb
+++ src/namespaces/mixins/white_list_hasher.rb
@@ -13,18 +13,16 @@ module Metalware
 
         private
 
-        def white_list_hash_methods
-          white_list_for_hasher.each_with_object({}) do |method, memo|
+        def white_list_hash_methods(h = {})
+          white_list_for_hasher.each_with_object(h) do |method, memo|
             memo[method] = send(method)
-            memo
           end
         end
 
         def merge_white_list_recursive_methods(h = {})
-          rwl = recursive_white_list_for_hasher
-          rwl.each_with_object(h) do |method, memo|
+          recursive_white_list_for_hasher
+            .each_with_object(h) do |method, memo|
             memo[method] = send(method).to_h
-            memo
           end
         end
 
