620769dca53ca92d4dfa7bca16e48c80005117c9
Vaughan Jones <vaughanlewisjones@icloud.com>
2018-01-04 17:10:05 +0000

    Fix `render_template`

--- src/vm.rb
+++ src/vm.rb
@@ -93,8 +93,7 @@ module Metalware
 
     def render_template(type)
       path = "/var/lib/metalware/repo/libvirt/#{type}.xml"
-      node = alces.nodes.find_by_name(@node)
-      templater = node ? node : alces
+      templater = @node ? @node : alces
       templater.render_erb_template(File.read(path))
     end
   end
