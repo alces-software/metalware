63f5b904c538c0a66e9d3347e9897fd3b57632d4
Bob Whitelock <bob.whitelock1@gmail.com>
2018-01-18 12:40:23 +0000

    Add test for Plugin.enabled_question_identifier
    This already passes, but we want to ensure the identifier of the
    generated question for whether the plugin is enabled looks like this:
    since this will be somewhat user-visible/accessible it should be clear
    what its purpose is, and we want to keep a standard format for potential
    future addition of other internal questions.

---
+++ spec/plugins/plugin_spec.rb
@@ -0,0 +1,13 @@
+
+RSpec.describe Metalware::Plugins::Plugin do
+  describe '#enabled_question_identifier' do
+    it 'gives correct identifier for generated plugin enabled question' do
+      plugin_dir_path = Pathname.new('path/to/some-plugin')
+      plugin = described_class.new(plugin_dir_path)
+
+      expect(
+        plugin.enabled_question_identifier
+      ).to eq 'metalware_internal--plugin_enabled--some-plugin'
+    end
+  end
+end
