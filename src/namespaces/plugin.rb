3694eca226fd3f52744338bb021554bbe37548c2
Bob Whitelock <bob.whitelock1@gmail.com>
2018-01-24 15:15:58 +0000

    Merge and make available plugin config in namespace
    This commit makes the merged plugin config available within the Plugin
    namespace. This config is loaded in the same order as for the containing
    node for the Plugin namespace, but using the config files nested within
    the plugin's own config directory as the input.
    
    The standard HashMergerNamespace class (as a parent for the Plugin
    namespace), and a new PluginConfig hash merger class, are used to
    perform this config merging.

--- src/namespaces/plugin.rb
+++ src/namespaces/plugin.rb
@@ -1,10 +1,47 @@
 
 # frozen_string_literal: true
 
+require 'namespaces/hash_merger_namespace'
+
+# A Plugin namespace contains the values configured for a particular plugin for
+# a particular node.
+
 module Metalware
   module Namespaces
-    Plugin = Struct.new(:plugin) do
+    class Plugin < HashMergerNamespace
       delegate :name, to: :plugin
+
+      def initialize(node_namespace, plugin)
+        @node_namespace = node_namespace
+        @plugin = plugin
+        alces = node_namespace.send(:alces)
+        super(alces, plugin.name)
+      end
+
+      def config
+        @config ||= run_hash_merger(plugin_config_hash_merger)
+      end
+
+      private
+
+      attr_reader :node_namespace, :plugin
+
+      def plugin_config_hash_merger
+        HashMergers::PluginConfig.new(
+          metal_config: alces.send(:metal_config),
+          plugin: plugin
+        )
+      end
+
+      def hash_merger_input
+        # The plugin config should be merged in the same order as specified in
+        # the containing node namespace.
+        node_namespace.send(:hash_merger_input)
+      end
+
+      def additional_dynamic_namespace
+        {}
+      end
     end
   end
 end
