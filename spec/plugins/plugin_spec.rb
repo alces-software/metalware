683c349ff4390474c6ae40957cc4839de3feb0a4
Bob Whitelock <bob.whitelock1@gmail.com>
2018-01-24 13:02:45 +0000

    Extract setup code in Plugin test

--- spec/plugins/plugin_spec.rb
+++ spec/plugins/plugin_spec.rb
@@ -1,12 +1,12 @@
 
 RSpec.describe Metalware::Plugins::Plugin do
+  let :plugin_dir_path { Pathname.new('/path/to/some-plugin') }
+  subject { described_class.new(plugin_dir_path) }
+
   describe '#enabled_question_identifier' do
     it 'gives correct identifier for generated plugin enabled question' do
-      plugin_dir_path = Pathname.new('path/to/some-plugin')
-      plugin = described_class.new(plugin_dir_path)
-
       expect(
-        plugin.enabled_question_identifier
+        subject.enabled_question_identifier
       ).to eq 'metalware_internal--plugin_enabled--some-plugin'
     end
   end
