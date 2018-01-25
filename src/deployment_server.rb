bb25743f2e4478b2d94666b0de467a1a9c3b6da5
Bob Whitelock <bob.whitelock1@gmail.com>
2018-01-25 16:08:46 +0000

    DRY up determining build file locations
    Previously this was duplicated, as we independently determined the
    location we should render the build files to and the URL they should be
    retrieved from, although the latter will depend on the former. These
    both happened to give consistent results at the moment, however that
    might not always be the case. This commit changes the latter to
    determine the URL using the former.

--- src/deployment_server.rb
+++ src/deployment_server.rb
@@ -46,9 +46,11 @@ module Metalware
         url "exec/kscomplete.php?name=#{node_name}" if node_name
       end
 
-      def build_file_url(files_dir, files_section, file_name)
-        path = File.join(files_dir, files_section.to_s, file_name)
-        url path
+      def build_file_url(*args)
+        rendered_files_path = Pathname.new(FilePath.rendered_build_file_path(*args))
+        rendered_files_root = Pathname.new(Constants::RENDERED_DIR_PATH)
+        relative_path = rendered_files_path.relative_path_from(rendered_files_root)
+        url relative_path
       end
 
       def build_interface
