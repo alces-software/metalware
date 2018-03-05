5f001c04d0e9b617e146089d962e30986de53a50
Bob Whitelock <bob.whitelock1@gmail.com>
2018-03-05 11:34:23 +0000

    Fix console using wrong host (fixes #326)
    Turns out my earlier comment was correct, and there is no reason
    `console` should use a different host from `ipmi` and `power`. Using
    just the host name (which it looks like we have always done, not sure if
    that worked in some cases?) was incorrect and doesn't work. See
    https://github.com/alces-software/metalware/issues/326.

--- src/commands/console.rb
+++ src/commands/console.rb
@@ -41,12 +41,9 @@ module Metalware
       end
 
       def console_command(type)
-        # XXX In Console we use `$node_name` as the host when running
-        # `ipmitool`, but in Ipmi and Power we use `$node_name.bmc` - is there
-        # any reason for this? Does this have any different effect?
         create_ipmitool_command(
           node,
-          host: node.name,
+          host: "#{node.name}.bmc",
           arguments: "-e '&' sol #{type}"
         )
       end
