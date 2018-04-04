b2d0ce073fe3dc1d064f650f2248c1b312e1fa53
atoghill <andrew.toghill@alces-software.com>
2018-04-04 11:45:17 +0100

    Resolve test errors. Only converts the default to a string if the value isn't nil

--- src/configurator/question.rb
+++ src/configurator/question.rb
@@ -60,7 +60,7 @@ module Metalware
 
       def default_input
         return human_readable_boolean_default if type.boolean?
-        default.to_s
+        default.nil? ? default : default.to_s
       end
 
       # Default for a boolean question which has a previous answer should be
