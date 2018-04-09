0d4f97d5f8766fb2c944c5256b90e9bd4d53b2e5
william <william.mccumstie@outlook.com>
2018-04-09 12:41:40 +0100

    Move file handling into an AssetLoader class
    This new class will perform the file handling. This ensures the method
    for loading an asset is stored with the file path itself.
    
    This sure help prevent things from getting out of sync when the named
    assessor methods are added. It will also implement a file cache.

--- src/namespaces/asset_array.rb
+++ src/namespaces/asset_array.rb
@@ -3,19 +3,39 @@
 module Metalware
   module Namespaces
     class AssetArray
+      class AssetLoader
+        def initialize(path)
+          @path = path
+        end
+
+        def data
+          load
+        end
+
+        private
+
+        attr_reader :path
+
+        def load
+          Data.load(path)
+        end
+      end
+
       include Enumerable
 
       def initialize
-        @files = Dir.glob(FilePath.asset('*'))
+        @asset_loaders = Dir.glob(FilePath.asset('*')).map do |path|
+          AssetLoader.new(path)
+        end
       end
 
       def [](index)
-        Data.load(files[index])
+        asset_loaders[index].data
       end
 
       private
 
-      attr_reader :files
+      attr_reader :asset_loaders
     end
   end
 end
