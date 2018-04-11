c986231d9042bbe476cf9bfac08092d1e020bf35
william <william.mccumstie@outlook.com>
2018-04-11 16:16:15 +0100

    Name the block to be explicitly the validator

--- src/utils/editor.rb
+++ src/utils/editor.rb
@@ -18,12 +18,12 @@ module Metalware
           ENV['VISUAL'] || ENV['EDITOR'] || DEFAULT_EDITOR
         end
 
-        def open_copy(source, destination, &b)
+        def open_copy(source, destination, &validator)
           name = File.basename(source, '.*')
           file = Tempfile.new(name)
           FileUtils.cp(source, file.path)
           open(file.path)
-          raise_error_if_validation_fails(file.path, &b) if b
+          raise_if_validation_fails(file.path, &validator) if validator
           FileUtils.cp(file.path, destination)
         ensure
           file.close
@@ -32,7 +32,7 @@ module Metalware
 
         private
 
-        def raise_error_if_validation_fails(path)
+        def raise_if_validation_fails(path)
           return if yield path
           raise ValidationFailure, <<-EOF.squish
             The edited file is invalid
