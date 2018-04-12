e3d3d92eae6b5a311d34a2aad5027c3e9bbd73a2
atoghill <andrew.toghill@alces-software.com>
2018-04-12 12:19:38 +0100

    Add tests for deleting an asset

---
+++ spec/commands/asset/delete_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'cache/asset'
+require 'filesystem'
+require 'commands'
+require 'utils'
+
+RSpec.describe Metalware::Commands::Asset::Delete do
+
+  it 'errors if the asset doesnt exist' do
+    expect do
+      Metalware::Utils.run_command(described_class,
+                                    'missing-type',
+                                    'name',
+                                    stderr: StringIO.new)
+    end.to raise_error(Metalware::InvalidInput)
+  end
+
+  context 'when using a saved asset' do
+    before do
+      FileSystem.root_setup do |fs|
+        fs.with_minimal_repo
+      end
+    end
+
+    let :asset { 'saved-asset' }
+    let :asset_path { Metalware::FilePath.asset(asset) }
+    let :asset_content { { key: 'value' } }
+
+    before :each { Metalware::Data.dump(asset_path, asset_content) }
+
+    def run_command
+      Metalware::Utils.run_command(described_class,
+                                    asset,
+                                    stderr: StringIO.new)
+    end
+    
+    it 'deletes the asset file' do
+      run_command
+    end
+  end
+end
