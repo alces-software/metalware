210615d87ab622faba943f41b4946c8e8798928d
atoghill <andrew.toghill@alces-software.com>
2018-04-12 12:19:02 +0100

    Add test for unassigning an asset from a node

--- spec/cache/asset_spec.rb
+++ spec/cache/asset_spec.rb
@@ -59,4 +59,12 @@ RSpec.describe Metalware::Cache::Asset do
       expect(new_cache.asset_for_node(node)).to eq(asset_name)
     end
   end
+
+  describe '#unassign_asset' do
+    it 'unassigns the asset from the node' do
+      cache.unassign_asset('asset_test')
+      new_cache = Metalware::Cache::Asset.new
+      expect(new_cache.data).not_to eq(content)
+    end
+  end
 end
