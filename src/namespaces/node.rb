f32f3180b3d2c7f16b27b10e3ab48cdfcd56db19
atoghill <andrew.toghill@alces-software.com>
2018-04-12 13:17:17 +0100

    Remove trailing whitespace

--- src/namespaces/node.rb
+++ src/namespaces/node.rb
@@ -90,7 +90,7 @@ module Metalware
         @asset ||= begin
           asset_name = alces.asset_cache.asset_for_node(name)
           return unless asset_name
-          alces.asset.find_by_name(asset_name)  
+          alces.asset.find_by_name(asset_name)
         end
       end
 
