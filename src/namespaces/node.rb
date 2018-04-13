245f0273201df69e56f64f2b9107aed6df9db002
atoghill <andrew.toghill@alces-software.com>
2018-04-13 17:27:32 +0100

    Change asset openstruct definition to be clearer

--- src/namespaces/node.rb
+++ src/namespaces/node.rb
@@ -90,7 +90,10 @@ module Metalware
         @asset ||= begin
           asset_name = alces.asset_cache.asset_for_node(self)
           return unless asset_name
-          OpenStruct.new(name: asset_name, data: alces.asset.find_by_name(asset_name))
+          OpenStruct.new(
+            name: asset_name,
+            data: alces.asset.find_by_name(asset_name)
+          )
         end
       end
 
