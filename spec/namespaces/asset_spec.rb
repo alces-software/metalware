953ccfe8ae1dfc6c86c82cea27044e5b34b5239e
william <william.mccumstie@outlook.com>
2018-04-16 13:16:08 +0100

    Create test suite for referencing assets
    This commit creates the basic test setup for testing if assets can
    reference each other. Currently the ":<asset_name>" strings will be
    returned in the raw format.
    
    Eventually, this syntax will be converted to the other assets hash.

--- spec/namespaces/asset_spec.rb
+++ spec/namespaces/asset_spec.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
+require 'alces_utils'
 require 'namespaces/alces'
 
 RSpec.describe Metalware::Namespaces::Asset do
+  include AlcesUtils
+
   let :metal_ros do
     Metalware::HashMergers::MetalRecursiveOpenStruct
   end
@@ -26,6 +29,34 @@ RSpec.describe Metalware::Namespaces::Asset do
         alces.domain.render_erb_template(template)
       end
       expect(asset.key).to eq(unrendered_erb)
+    end
+  end
+
+  context 'when referencing other asset (":<asset_name>")' do
+    let :asset1 { alces.assets.find_by_name(asset1_name) }
+    let :asset2 { alces.assets.find_by_name(asset2_name) }
+    let :asset1_name { 'test-asset1' }
+    let :asset2_name { 'test-asset2' }
+    let :asset1_data do
+      {
+        key: "#{asset1_name}-data",
+        link: ":#{asset2_name}",
+      }
+    end
+    let :asset2_data do
+      {
+        key: "#{asset2_name}-data",
+        link: ":#{asset1_name}",
+      }
+    end
+
+    AlcesUtils.mock(self, :each) do
+      create_asset(asset1_name, asset1_data)
+      create_asset(asset2_name, asset2_data)
+    end
+
+    it 'can still be converted to a hash' do
+      expect(asset1.to_h).to eq(asset1_data)
     end
   end
 end
