1472bcdb04a42d3e349285ed9cf1d91a5ccd5b9f
atoghill <andrew.toghill@alces-software.com>
2018-04-24 17:09:05 +0100

    Check if the asset file exists

---
+++ src/command_helpers/ensure_asset_exists.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Metalware
+  module CommandHelpers
+    module EnsureAssetExists
+      def post_setup
+        super
+        error_if_asset_file_does_not_exist(asset_path)
+      end 
+
+      def error_if_asset_file_does_not_exist(asset_name)
+        record_name = File.basename(asset_path, '.*')
+        return if File.exist?(asset_path)
+        raise InvalidInput, <<-EOF.squish
+          The "#{asset_name}" record does not exist
+        EOF
+      end
+    end   
+  end
+end
