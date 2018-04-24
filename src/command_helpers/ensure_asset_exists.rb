0e616219d2809477f4326eeb398951d9f35330ad
atoghill <andrew.toghill@alces-software.com>
2018-04-24 17:21:06 +0100

    Remove shadowed local variable

--- src/command_helpers/ensure_asset_exists.rb
+++ src/command_helpers/ensure_asset_exists.rb
@@ -6,10 +6,9 @@ module Metalware
       def post_setup
         super
         error_if_asset_file_does_not_exist(asset_path)
-      end 
+      end
 
       def error_if_asset_file_does_not_exist(asset_name)
-        asset_name = File.basename(asset_path, '.*')
         return if File.exist?(asset_path)
         raise InvalidInput, <<-EOF.squish
           The "#{asset_name}" record does not exist
