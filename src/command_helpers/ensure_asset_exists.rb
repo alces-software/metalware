e4225404fe8ca99d5039e96eb07a0389cab6d3b0
atoghill <andrew.toghill@alces-software.com>
2018-04-25 10:04:12 +0100

    Combine the two methods

--- src/command_helpers/ensure_asset_exists.rb
+++ src/command_helpers/ensure_asset_exists.rb
@@ -5,10 +5,6 @@ module Metalware
     module EnsureAssetExists
       def post_setup
         super
-        error_if_asset_file_does_not_exist(asset_path)
-      end
-
-      def error_if_asset_file_does_not_exist(asset_name)
         return if File.exist?(asset_path)
         raise InvalidInput, <<-EOF.squish
           The "#{asset_name}" record does not exist
