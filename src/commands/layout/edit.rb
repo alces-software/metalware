3c23d47e0784c8432c5b264b25133ddc125f40a9
atoghill <andrew.toghill@alces-software.com>
2018-04-27 12:49:23 +0100

    Add the layout edit command

---
+++ src/commands/layout/edit.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Metalware
+  module Commands
+    module Layout
+      class Edit < CommandHelpers::RecordEditor
+        private
+      end
+    end
+  end
+end
