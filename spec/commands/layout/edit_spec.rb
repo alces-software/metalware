cf77b3f2c6d204471672eb531dc0581f86714f2c
atoghill <andrew.toghill@alces-software.com>
2018-04-30 17:27:22 +0100

    Utilise the shared example for record edit commands

--- spec/commands/layout/edit_spec.rb
+++ spec/commands/layout/edit_spec.rb
@@ -1,39 +1,19 @@
 # frozen_string_literal: true
 
 require 'alces_utils'
+require 'shared_examples/record_edit_command'
 
 RSpec.describe Metalware::Commands::Layout::Edit do
   include AlcesUtils
   before { allow(Metalware::Utils::Editor).to receive(:open) }
 
-  it 'errors if the asset doesnt exist' do
-    expect do
-      Metalware::Utils.run_command(described_class,
-                                   'missing-layout',
-                                   stderr: StringIO.new)
-    end.to raise_error(Metalware::MissingRecordError)
+  let(:record_name) { 'layout' }
+  let(:record_path) { Metalware::Records::Layout.path(record_name) }
+
+  AlcesUtils.mock(self, :each) do
+    FileSystem.root_setup(&:with_minimal_repo)
+    create_layout(record_name, {})
   end
 
-  context 'when using a saved layout' do
-    let(:saved_layout) { 'saved-layout' }
-    let(:test_content) { { key: 'value' } }
-    let(:layout_path) { Metalware::Records::Layout.path(saved_layout) }
-
-    AlcesUtils.mock(self, :each) do
-      FileSystem.root_setup(&:with_minimal_repo)
-      create_layout(saved_layout, test_content)
-    end
-
-    def run_command
-      Metalware::Utils.run_command(described_class,
-                                   saved_layout,
-                                   stderr: StringIO.new)
-    end
-
-    it 'calls for the saved layout to be opened and copied' do
-      expect(Metalware::Utils::Editor).to receive(:open_copy)
-        .with(layout_path, layout_path)
-      run_command
-    end
-  end
+  it_behaves_like 'record edit command'
 end
