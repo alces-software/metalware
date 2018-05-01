b96b7f566ac39f1e0a9926e60bcf657c10efdd6b
atoghill <andrew.toghill@alces-software.com>
2018-05-01 11:21:02 +0100

    Move relevant methods from LayoutEditor -> AssetEditor

--- src/command_helpers/asset_editor.rb
+++ src/command_helpers/asset_editor.rb
@@ -6,6 +6,28 @@ module Metalware
   module CommandHelpers
     class AssetEditor < LayoutEditor
       private
+
+      attr_accessor :node
+
+      def unpack_node_from_options
+        return unless options.node
+        self.node = alces.nodes.find_by_name(options.node)
+        raise_error_if_node_is_missing
+      end
+
+      def assign_asset_to_node_if_given(asset_name)
+        return unless node
+        Cache::Asset.update do |cache|
+          cache.assign_asset_to_node(asset_name, node)
+        end
+      end
+
+      def raise_error_if_node_is_missing
+        return if node
+        raise InvalidInput, <<-EOF
+          Unable to find node: #{options.node}
+        EOF
+      end
     end
   end
 end
--- src/command_helpers/layout_editor.rb
+++ src/command_helpers/layout_editor.rb
@@ -7,32 +7,10 @@ module Metalware
     class LayoutEditor < BaseCommand
       private
 
-      attr_accessor :node
-
-      def unpack_node_from_options
-        return unless options.node
-        self.node = alces.nodes.find_by_name(options.node)
-        raise_error_if_node_is_missing
-      end
-
-      def assign_asset_to_node_if_given(asset_name)
-        return unless node
-        Cache::Asset.update do |cache|
-          cache.assign_asset_to_node(asset_name, node)
-        end
-      end
-
       def copy_and_edit_record_file
         Utils::Editor.open_copy(source, destination) do |edited_path|
           Validation::Asset.valid_file?(edited_path)
         end
-      end
-
-      def raise_error_if_node_is_missing
-        return if node
-        raise InvalidInput, <<-EOF
-          Can not find node: #{options.node}
-        EOF
       end
 
       def source
