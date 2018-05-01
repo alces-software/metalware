472e292b3ed9336eb5be519047d83b0aa3b5a0f9
atoghill <andrew.toghill@alces-software.com>
2018-05-01 11:16:57 +0100

    Create the AssetEditor class, inherited from LayoutEditor

---
+++ src/command_helpers/asset_editor.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'command_helpers/layout_editor'
+
+module Metalware
+  module CommandHelpers
+    class AssetEditor < LayoutEditor
+      private
+    end
+  end
+end
