964e825a457095c177d04557b63634764f041436
atoghill <andrew.toghill@alces-software.com>
2018-05-02 11:06:41 +0100

    Test for adding from a layout

--- spec/shared_examples/record_add_command.rb
+++ spec/shared_examples/record_add_command.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
+require 'alces_utils'
+
 RSpec.shared_examples 'record add command' do
+  include AlcesUtils
   # Stops the editor from running the bash command
   before { allow(Metalware::Utils::Editor).to receive(:open) }
+
+  let(:type) { 'rack' }
+  let(:saved_record_name) { 'saved-record' }
 
   xit 'errors if the type does not exist' do
     expect do
@@ -15,9 +21,6 @@ RSpec.shared_examples 'record add command' do
 
   context 'when using the rack type' do
     before { FileSystem.root_setup(&:with_asset_types) }
-
-    let(:type) { 'rack' }
-    let(:saved_record_name) { 'saved-record' }
 
     let(:type_path) { Metalware::FilePath.asset_type(type) }
 
@@ -47,6 +50,29 @@ RSpec.shared_examples 'record add command' do
         run_command(type)
       end.to raise_error(Metalware::InvalidInput)
         .with_message(/is not a valid/)
+    end
+  end
+
+  context 'with a layout' do
+    AlcesUtils.mock(self, :each) do
+      FileSystem.root_setup(&:with_minimal_repo)
+      create_layout(layout, {})
+    end
+
+    let(:layout) { 'rack-layout' }
+    let(:layout_path) { Metalware::FilePath.layout(type.pluralize, layout) }
+
+    def run_command(record_name = saved_record_name)
+      Metalware::Utils.run_command(described_class,
+                                   layout,
+                                   record_name,
+                                   stderr: StringIO.new)
+    end
+
+    it 'calls for the record to be opened and copied' do
+      expect(Metalware::Utils::Editor).to receive(:open_copy)
+        .with(layout_path, record_path)
+      run_command
     end
   end
 end
