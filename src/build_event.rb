d6d0618d916bcdf1dfa0a562ee23dac6bd7d45df
william <william.mccumstie@outlook.com>
2018-05-02 17:52:59 +0100

    Join the dead threads NOT the alive
    Turns out `delete_if` and `delete` return different values. `delete`
    returns the deleted values, where `delete_if` returns the original
    object.
    
    This meant the alive threads where being joined instead of the dead
    ones. This was what caused the build to hang for `local`.
    
    Also the `BuildEvent` needed to be updated to use `loop while` instead
    of `loop if`. I don't know what `loop if` does, but what ever it is, it
    causes the tests to fail.

--- src/build_event.rb
+++ src/build_event.rb
@@ -22,7 +22,7 @@ module Metalware
 
     def run_all_complete_hooks
       nodes.each { |node| run_hook(node, 'complete') }
-      loop if hook_active?
+      loop while hook_active?
     end
 
     def build_complete?
@@ -31,8 +31,9 @@ module Metalware
     end
 
     def hook_active?
-      build_threads.delete_if { |th| !th.alive? }
-                   .each(&:join)
+      build_threads.delete_if do |th|
+        (!th.alive?).tap { |dead| th.join if dead }
+      end
       !build_threads.empty?
     end
 
