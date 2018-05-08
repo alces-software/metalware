91dceb7f36edbfdebfd3f9b03a8b10fbd2973ac5
william <william.mccumstie@outlook.com>
2018-05-08 14:33:57 +0100

    Create the Filter asset command

---
+++ src/commands/asset/tool/filter.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Metalware
+  module Commands
+    module Asset
+      module Tool
+        class Filter < CommandHelpers::BaseCommand
+          def run
+            puts 'hello world'
+          end
+        end
+      end
+    end
+  end
+end
