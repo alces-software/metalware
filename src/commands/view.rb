8821924a6e956c93217eb4cf73e84a7cca3f1b20
william <william.mccumstie@outlook.com>
2018-06-20 14:16:32 +0100

    Remove the `alces_command` helper
    The `alces_command` helper was originally created to prevent the use of
    `eval`. However this has become unworkable as it requires parsing the
    input.
    
    Instead, `instance_eval` can be used as it allows any ruby code to be
    ran through the `view` command.
    
    The `to_json` methods have been added to `MetalROS` and `AssetArray`.
    This makes them convert nicely by using their underlining object.

--- src/commands/view.rb
+++ src/commands/view.rb
@@ -2,32 +2,20 @@
 # frozen_string_literal: true
 
 require 'json'
-require 'command_helpers/alces_command'
 
 module Metalware
   module Commands
     class View < CommandHelpers::BaseCommand
       private
 
-      include CommandHelpers::AlcesCommand
+      attr_reader :command
 
-      ARRAY_TYPES = [Array, Namespaces::AssetArray].freeze
-
-      def run
-        pretty_print_json(cli_input_object.to_json)
+      def setup
+        @command = args.first
       end
 
-      def cli_input_object
-        data = alces_command
-        if data.is_a?(Namespaces::MetalArray)
-          data
-        elsif ARRAY_TYPES.include?(data.class)
-          data.map(&:to_h)
-        elsif data.respond_to?(:to_h)
-          data.to_h
-        else
-          data
-        end
+      def run
+        pretty_print_json(alces.instance_eval(command).to_json)
       end
 
       def pretty_print_json(json)
